6849ee2c0c7f7c4d012a8b5c83bc9ab8dcb649b5
Piotr Blaszczak <piotrblaszczakooo@gmail.com>
2020-06-16 14:23:42 +0200

    Updates

---
+++ precache-manifest.6d7b1f76caf71cb585afd636aa5a0b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9e74d985615aaa1e0b73de3df2d3e79",
+    "url": "/Calendary-React/index.html"
+  },
+  {
+    "revision": "6d9b512811e48ee16ee9",
+    "url": "/Calendary-React/static/css/main.2a20d5b3.chunk.css"
+  },
+  {
+    "revision": "8600101fa4f53deda973",
+    "url": "/Calendary-React/static/js/2.a6d9ee11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Calendary-React/static/js/2.a6d9ee11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9b512811e48ee16ee9",
+    "url": "/Calendary-React/static/js/main.7ffcc084.chunk.js"
+  },
+  {
+    "revision": "ea6044c66121538c4cd4",
+    "url": "/Calendary-React/static/js/runtime-main.e2d3de37.js"
+  }
+]);
